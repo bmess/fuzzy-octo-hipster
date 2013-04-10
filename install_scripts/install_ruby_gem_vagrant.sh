66c52420b765f1a420f9ac04ae2144253d6406dd
bobby <bobby@jackprogram.com>
2013-04-10 15:45:22 -0700

    Update install_ruby_gem_vagrant.sh
    Updated to check for 64 vs 32 bit
    
    NOT TESTED!
    
    Good luck!

--- install_scripts/install_ruby_gem_vagrant.sh
+++ install_scripts/install_ruby_gem_vagrant.sh
@@ -5,13 +5,29 @@
 # Install gem
 sudo apt-get install gem ruby-dev
 
-# Now *in gem*, install vagrant
-if [ -f ./vagrant_x86_64.deb ];
+if [ $(uname) == 'x86_64' ];
 then
-    echo "file exists, trying to install now"
-    sudo dpkg --install vagrant_x86_64.deb
-else 
-    echo "Trying to download x86_64 version to install..."
-    wget http://files.vagrantup.com/packages/64e360814c3ad960d810456add977fd4c7d47ce6/vagrant_x86_64.deb
-    sudo dpkg --install vagrant_x86_64.deb
+    echo "64 bit detected"
+    # Now *in gem*, install vagrant
+    if [ -f ./vagrant_x86_64.deb ];
+    then
+        echo "file exists, trying to install now"
+        sudo dpkg --install vagrant_x86_64.deb
+    else 
+        echo "Trying to download x86_64 version to install..."
+        wget http://files.vagrantup.com/packages/64e360814c3ad960d810456add977fd4c7d47ce6/vagrant_x86_64.deb
+        sudo dpkg --install vagrant_x86_64.deb
+    fi
+else
+    echo "32 bit detected"
+    # Now *in gem*, install vagrant
+    if [ -f ./vagrant_x86.deb ];
+    then
+        echo "file exists, trying to install now"
+        sudo dpkg --install vagrant_x86_64.deb
+    else 
+        echo "Trying to download i686 version to install..."
+        wget http://files.vagrantup.com/packages/64e360814c3ad960d810456add977fd4c7d47ce6/vagrant_i686.deb
+        sudo dpkg --install vagrant_i686.deb
+    fi
 fi
