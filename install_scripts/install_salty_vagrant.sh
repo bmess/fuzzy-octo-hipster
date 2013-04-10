11a540a6c4a3ce169d3b63b29749005288b9d5ff
bobby <bobby@jackprogram.com>
2013-04-10 15:40:50 -0700

    Update install_salty_vagrant.sh
    removed sudo so it actually works

--- install_scripts/install_salty_vagrant.sh
+++ install_scripts/install_salty_vagrant.sh
@@ -2,7 +2,7 @@
 
 cd ..
 
-sudo vagrant plugin install vagrant-salt
+vagrant plugin install vagrant-salt
 
-sudo vagrant box add precise64 http://files.vagrantup.com/precise64.box
+vagrant box add precise64 http://files.vagrantup.com/precise64.box
 
