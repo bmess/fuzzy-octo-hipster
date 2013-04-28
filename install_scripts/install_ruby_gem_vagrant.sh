2553f771fd0cebdf9f498cd1b4b14775b0e7e90d
bobby <bobby@jackprogram.com>
2013-04-27 23:13:41 -0700

    Yet another dumb typo
    Typos galore!  Fixed
    
    	modified:   install_scripts/install_ruby_gem_vagrant.sh

--- install_scripts/install_ruby_gem_vagrant.sh
+++ install_scripts/install_ruby_gem_vagrant.sh
@@ -7,7 +7,7 @@ sudo apt-get install gem ruby-dev
 
 if [ $(uname -m) == 'x86_64' ];
 then
-    echo "64 bit detected"
+    echo "64 bit detected, now installing 64-bit vbox"
     # Now *in gem*, install vagrant
     if [ -f ./vagrant_x86_64.deb ];
     then
@@ -19,12 +19,12 @@ then
         sudo dpkg --install vagrant_x86_64.deb
     fi
 else
-    echo "32 bit detected"
+    echo "32 bit detected, now installing 32-bit vbox"
     # Now *in gem*, install vagrant
     if [ -f ./vagrant_i686.deb ];
     then
         echo "file exists, trying to install now"
-        sudo dpkg --install vagrant_x86_64.deb
+        sudo dpkg --install vagrant_i686.deb
     else 
         echo "Trying to download i686 version to install..."
         wget http://files.vagrantup.com/packages/64e360814c3ad960d810456add977fd4c7d47ce6/vagrant_i686.deb
