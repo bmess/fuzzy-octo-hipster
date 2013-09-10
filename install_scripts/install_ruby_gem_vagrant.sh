6c4d9450ad7e27a68ef22fdf6f467941eb7e5e22
bobby messemer <bmessemer@cars.com>
2013-09-10 16:13:12 -0700

    Updated for newest releases
    	modified:   install_scripts/install_ruby_gem_vagrant.sh
    	modified:   install_scripts/install_salty_vagrant.sh

--- install_scripts/install_ruby_gem_vagrant.sh
+++ install_scripts/install_ruby_gem_vagrant.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Keeping it simple so I can keep it lazy
+# Keeping it simple.  We're going to check the architecture of the machine and
+# download the proper version of the vagrant binary.
 
 # Install gem
 sudo apt-get install gem ruby-dev
@@ -9,25 +10,25 @@ if [ $(uname -m) == 'x86_64' ];
 then
     echo "64 bit detected, now installing 64-bit vbox"
     # Now *in gem*, install vagrant
-    if [ -f ./vagrant_x86_64.deb ];
+    if [ -f ./vagrant_1.3.1_x86_64.deb ];
     then
         echo "file exists, trying to install now"
-        sudo dpkg --install vagrant_x86_64.deb
-    else 
+        sudo dpkg --install vagrant_1.3.1_x86_64.deb
+    else
         echo "Trying to download x86_64 version to install..."
-        wget http://files.vagrantup.com/packages/64e360814c3ad960d810456add977fd4c7d47ce6/vagrant_x86_64.deb
-        sudo dpkg --install vagrant_x86_64.deb
+        wget http://files.vagrantup.com/packages/b12c7e8814171c1295ef82416ffe51e8a168a244/vagrant_1.3.1_x86_64.deb
+        sudo dpkg --install vagrant_1.3.1_x86_64.deb
     fi
 else
     echo "32 bit detected, now installing 32-bit vbox"
     # Now *in gem*, install vagrant
-    if [ -f ./vagrant_i686.deb ];
+    if [ -f ./vagrant_1.3.1_i686.deb ];
     then
         echo "file exists, trying to install now"
-        sudo dpkg --install vagrant_i686.deb
-    else 
+        sudo dpkg --install vagrant_1.3.1_i686.deb
+    else
         echo "Trying to download i686 version to install..."
-        wget http://files.vagrantup.com/packages/64e360814c3ad960d810456add977fd4c7d47ce6/vagrant_i686.deb
-        sudo dpkg --install vagrant_i686.deb
+        wget http://files.vagrantup.com/packages/b12c7e8814171c1295ef82416ffe51e8a168a244/vagrant_1.3.1_i686.deb
+        sudo dpkg --install vagrant_1.3.1_i686.deb
     fi
 fi
